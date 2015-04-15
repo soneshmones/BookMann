d4c8f37a5512f8735b176e5cd9d9ca4b061df53c
Phil <pbdeutsch@gmail.com>
2015-04-14 21:33:25 -0400

    Added grey/white toggle support on teacher schedules

--- free-time/free-time.js
+++ free-time/free-time.js
@@ -9,3 +9,14 @@ Router.map(function(){
 		path: '/'
 	});
 });
+
+function setColor(btn,color){
+    
+    var property=document.getElementById(btn);
+   if (window.getComputedStyle(property).backgroundColor == 'rgb(244, 113, 33)') {
+      property.style.backgroundColor=color;
+   }
+    else {
+      property.style.backgroundColor = "#f47121";
+    }
+  };
